223ab87a3e46b963602fad5bd362882b5aad3a18
정호진 <jeonghojin@jeonghojin-ui-MacBookAir.local>
2026-02-16 18:22:36 +0900

    compile all file

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,7 @@
 import adapter from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// 먼저 preprocess 설정
-	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter() // Netlify용 어댑터
 		// 필요시 다른 설정 추가 가능
