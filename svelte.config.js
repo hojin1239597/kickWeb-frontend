0a71616d91f72d88f89fd74e9c5c7ac285814512
정호진 <jeonghojin@jeonghojin-ui-MacBookAir.local>
2026-02-16 18:15:26 +0900

    compile scss file and svelte.config,js file -v2

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
-import adapter from '@sveltejs/adapter-netlify'; // 추가
+import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// 먼저 preprocess 설정
+	preprocess: preprocess(),
+
 	kit: {
-		adapter: adapter()
-	},
-	preprocess: vitePreprocess()
+		adapter: adapter() // Netlify용 어댑터
+		// 필요시 다른 설정 추가 가능
+	}
 };
 
 export default config;
